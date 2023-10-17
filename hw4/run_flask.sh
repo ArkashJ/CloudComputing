eff59141113384327f88a09ca701f72ccbb38359
Arkash Jain <86805856+ArkashJ@users.noreply.github.com>
2023-10-17 12:42:40 -0400

    script modified

--- hw4/run_flask.sh
+++ hw4/run_flask.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-flask --app main run --host=0.0.0.0 --port=8080
+
+echo "installing python3"
+sudo apt-get install python3-pip -y
+
+echo "copying files from bucket"
+sudo gsutil -m cp -r gs://ds-561-hw3/hw4-files /home/arkjain
+
+echo "installing requirements"
+sudo cd hw4-files
+
+sudo pip3 install -r requirements.txt
+echo "running flask app"
+python3 -m flask --app main run --host=0.0.0.0 --port=5000 &
