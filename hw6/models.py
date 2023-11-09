8051681343026fbb32e632943fbca6ec913272aa
Arkash Jain <86805856+ArkashJ@users.noreply.github.com>
2023-11-08 20:07:48 -0500

    /

--- hw6/models.py
+++ hw6/models.py
@@ -19,32 +19,32 @@ def pre_process_data():
     requests.drop_duplicates(inplace=True)
     return requests
  
-def country_vs_ip():
-    requests = pre_process_data()    
-    countries = requests['Country']
-    client_ip = requests['Client_Ip']
-
-    def ip_to_number(ip):
-        parts = ip.split('.')
-        return int(parts[0]) * (256 * 256 * 256) + int(parts[1]) * (256 * 256) + int(parts[2]) * 256 + int(parts[3])
-
-    client_ip = client_ip.apply(lambda x: int(''.join([i for i in x if i.isdigit()])))
-
-    X_train, X_test, y_train, y_test = train_test_split(client_ip, countries, test_size=0.2)
-
-    # reshape to 2D array
-    X_train = X_train.values.reshape(-1, 1)
-    X_test = X_test.values.reshape(-1, 1)
-
-    # Use a decision tree classifier
-    clf = DecisionTreeClassifier()
-    clf.fit(X_train, y_train)
-    print(f"Accuracy for country versus IP: {clf.score(X_test, y_test)}")
-
-    #import random forest classifier
-    country_model = RandomForestClassifier(n_estimators=100, random_state=0)
-    country_model.fit(X_train, y_train)
-    print(f"Accuracy for country versus IP: {country_model.score(X_test, y_test)}")
+# def country_vs_ip():
+#     requests = pre_process_data()    
+#     countries = requests['Country']
+#     client_ip = requests['Client_Ip']
+#
+#     def ip_to_number(ip):
+#         parts = ip.split('.')
+#         return int(parts[0]) * (256 * 256 * 256) + int(parts[1]) * (256 * 256) + int(parts[2]) * 256 + int(parts[3])
+#
+#     client_ip = client_ip.apply(lambda x: int(''.join([i for i in x if i.isdigit()])))
+#
+#     X_train, X_test, y_train, y_test = train_test_split(client_ip, countries, test_size=0.2)
+#
+#     # reshape to 2D array
+#     X_train = X_train.values.reshape(-1, 1)
+#     X_test = X_test.values.reshape(-1, 1)
+#
+#     # Use a decision tree classifier
+#     clf = DecisionTreeClassifier()
+#     clf.fit(X_train, y_train)
+#     print(f"Accuracy for country versus IP: {clf.score(X_test, y_test)}")
+#
+#     #import random forest classifier
+#     country_model = RandomForestClassifier(n_estimators=100, random_state=0)
+#     country_model.fit(X_train, y_train)
+#     print(f"Accuracy for country versus IP: {country_model.score(X_test, y_test)}")
     
 def income_model():
     requests = pre_process_data()
@@ -69,7 +69,7 @@ def income_model():
    
     
 def main():
-    country_vs_ip()
+    #country_vs_ip()
     income_model()
 
 main()
