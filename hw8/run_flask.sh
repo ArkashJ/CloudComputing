6a550eeb78cbd86109d670796f1cec7e9beeaf37
Arkash Jain <86805856+ArkashJ@users.noreply.github.com>
2023-11-19 18:34:20 -0500

    adding the startup script bash file

---
+++ hw8/run_flask.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "installing python3"
+sudo apt-get install python3-pip -y
+echo "copying files from bucket"
+sudo gsutil -m cp -r gs://ds-561-hw8/hw8-files /home/arkjain
+curr_path="/home/arkjain/hw8-files"
+echo "installing requirements"
+cd $curr_path
+sudo pip3 install -r requirements.txt
+echo "running flask app"
+waitress-serve --host 0.0.0.0 --port=5000 main:app &
