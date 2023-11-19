07e4a7e4ad319d092da24366152b346d445d15ca
Arkash Jain <86805856+ArkashJ@users.noreply.github.com>
2023-11-19 18:33:06 -0500

    installing waitress

--- hw8/main.py
+++ hw8/main.py
@@ -1,20 +1,17 @@
-import os
-from flask import Flask, Request, Response, request
-from google.cloud import storage
 from typing import Optional
+
+from flask import Flask, Response, request
 from google.api_core import exceptions
-from google.cloud import pubsub_v1
-from google.cloud import logging
+from google.cloud import logging, pubsub_v1, storage
 
 app = Flask(__name__)
 
 
-def make_logging_client():
-    client = logging.Client()
-    log_name = "requester_countries_logs"
-    client.setup_logging()
-    logger = client.logger(log_name)
-    return logger
+client = logging.Client()
+log_name = "requester_countries_logs"
+client.setup_logging()
+logger = client.logger(log_name)
+
 
 def make_storage_client() -> storage.Client:
     client = storage.Client().create_anonymous_client()
@@ -26,18 +23,15 @@ def get_files_from_bucket(
     folder_name: str,
     file_name: str,
 ) -> Optional[str]:
-    logger = make_logging_client()
     try:
         client = make_storage_client()
         prefix: str = f"{folder_name}/{file_name}"
-        print("here------")
-        blobs = client.list_blobs(bucket_name, prefix=prefix)
-        print("Prefix", prefix, "blobs", blobs)
-        for blob in blobs:
-            print(f"blob name: {blob.name}")
-        logger.log_text(f"Accessing blob: {blob.name}, and retuning the data to the user {blob.download_as_string()}")
-        return Response(blob.download_as_string(), mimetype="text/html")
-    except:
+        bucket = client.get_bucket(bucket_name)
+        blob = bucket.get_blob(prefix)
+        blob_content = blob.download_as_string()
+        logger.log_text("Status: 200")
+        return Response(blob_content, status=200, mimetype="text/html")
+    except exceptions.NotFound:
         err_msg: str = "Error file not found"
         logger.log_text(f"Error file not found: {err_msg}, Status: 404")
         return Response(err_msg, status=404, mimetype="text/plain")
@@ -49,7 +43,6 @@ def get_bucket_path_fname(request_args: dict) -> Optional[str]:
     if name is None:
         return None
     print(name.split("/")[-3:])
-    logger = make_logging_client()
     return name.split("/")[-3:]
 
 
@@ -92,7 +85,6 @@ def receive_http_request(bucket_name, dir, file) -> Optional[Response]:
     topic_path = publisher.topic_path(
         "cloudcomputingcourse-398918", "banned_request_countries"
     )
-    logger = make_logging_client()
     print(bucket_name, dir, file)
     try:
         if request.method == "GET":
@@ -104,7 +96,9 @@ def receive_http_request(bucket_name, dir, file) -> Optional[Response]:
                 if check_if_country_is_enemy(country):
                     err_msg = f"The country of {country} is an enemy country"
                     future = publisher.publish(topic_path, err_msg.encode("utf-8"))
-                    logger.log_text(f"Error, the country of {country} is an enemy country, Status: 400")
+                    logger.log_text(
+                        f"Error, the country of {country} is an enemy country, Status: 400"
+                    )
                     return Response(err_msg, status=400, mimetype="text/plain")
 
             return get_files_from_bucket(bucket_name, dir, file)
@@ -115,7 +109,8 @@ def receive_http_request(bucket_name, dir, file) -> Optional[Response]:
             return Response(err_msg, status=501, mimetype="text/plain")
     except:
         err_msg = "Error, wrong HTTP Request Type"
-        #logger.log_text(f"Error, wrong HTTP Request Type, Status: 501")
+        # logger.log_text(f"Error, wrong HTTP Request Type, Status: 501")
         return Response(err_msg, status=501, mimetype="text/plain")
+
 
 # sudo journalctl -u google-startup-scripts.service -f
