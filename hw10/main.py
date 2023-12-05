0dc0ef49221c2acb0682eb666af452f35f96d28b
Arkash Jain <86805856+ArkashJ@users.noreply.github.com>
2023-12-05 18:47:40 -0500

    updating code

--- hw10/main.py
+++ hw10/main.py
@@ -170,6 +170,42 @@ def receive_http_request(bucket_name, dir, file) -> Optional[Response]:
         return Response(err_msg, status=501, mimetype="text/plain")
 
 
+def create_db():
+    try:
+        with pool.connect() as conn:
+            conn.execute(
+                sqlalchemy.text(
+                    """
+                    CREATE TABLE IF NOT EXISTS request (
+                        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                        country VARCHAR(255),
+                        gender ENUM('Male', 'Female'),
+                        age ENUM('0-16', '17-25', '26-35', '36-45', '46-55', '56-65', '66-75', '76+'),
+                        income ENUM('0-10k', '10k-20k', '20k-40k', '40k-60k', '60k-100k', '100k-150k', '150k-250k', '250k+'),
+                        is_banned BOOLEAN,
+                        client_ip VARCHAR(255),
+                        time_of_request TIMESTAMP)
+                    """
+                )
+            )
+            conn.execute(
+                sqlalchemy.text(
+                    """
+                CREATE TABLE IF NOT EXISTS request_time (
+                request_time TIMESTAMP,
+                requested_file VARCHAR(255),
+                error_code INT
+                    )
+                """
+                )
+            )
+            conn.commit()
+            conn.close()
+    except Exception as e:
+        console.log(f"Could not create database {e}")
+        return Response("Error", status=409, mimetype="text/plain")
+
+
 def make_database_and_publish_data(
     country,
     gender,
